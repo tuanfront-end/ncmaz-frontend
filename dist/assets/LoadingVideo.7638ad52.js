ead67ab1484e5c5cc897e747f2e6f70b1c62b4d2
Chis Nghiax <atuanvptd@gmail.com>
2022-07-07 21:13:33 +0700

    update

---
+++ dist/assets/LoadingVideo.7638ad52.js
@@ -0,0 +1,1 @@
+import{a as d,j as i}from"./main.3380b1f5.js";const o=({className:a="",childClassName:s="bg-neutral-100"})=>d("div",{className:`nc-LoadingVideo lds-ellipsis lds-ellipsis-video ${a}`,"data-nc-id":"LoadingVideo",children:[i("div",{className:s}),i("div",{className:s}),i("div",{className:s}),i("div",{className:s})]});export{o as L};
--- dist/assets/LoadingVideo.9a1555f1.js
+++
@@ -1,1 +0,0 @@
-import{a as d,j as i}from"./main.6c48ddcb.js";const o=({className:a="",childClassName:s="bg-neutral-100"})=>d("div",{className:`nc-LoadingVideo lds-ellipsis lds-ellipsis-video ${a}`,"data-nc-id":"LoadingVideo",children:[i("div",{className:s}),i("div",{className:s}),i("div",{className:s}),i("div",{className:s})]});export{o as L};
